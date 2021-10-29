6a2991744d12aad093a3e6ff2390da0841e36a2a
Shivanshudeveloper <developer.shivanshu@gmail.com>
2021-10-29 08:54:13 +0530

    Server

---
+++ server/routes/main.js
@@ -0,0 +1,95 @@
+const express = require("express");
+const router = express.Router();
+const { v4: uuidv4 } = require("uuid");
+// Getting Module
+const Products_Model = require("../models/Products");
+const MainStore_Model = require("../models/MainStore");
+
+
+const stripe = require("stripe")(
+  "sk_test_51IdwfeH8KzFo5uc9YHKzp2HOPkZJvH0ij0qhWeg0wQ17G73o5fVJYjMkWOfAmWUgjVZe0DesJvrQKbmAPSacXsVP00qMXnEqFr"
+);
+
+function isNumeric(str) {
+  if (typeof str != "string") return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
+}
+
+// TEST
+// @GET TEST
+// GET
+router.get("/test", (req, res) => {
+  res.send("Working");
+});
+
+router.post("/charges", async (req, res) => {
+  const { email, amount } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: amount * 100,
+    currency: "eur",
+    // Verify your integration in this guide by including this parameter
+    metadata: { integration_check: "accept_a_payment" },
+    receipt_email: email,
+  });
+
+  res.json({ client_secret: paymentIntent["client_secret"] });
+});
+
+// Database CRUD Operations
+// @POST Request to GET the People
+// GET
+router.get("/getallproductapi", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  Products_Model.find({})
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// Database CRUD Operations
+// @POST Request to GET the People
+// GET
+router.get("/getallproductsmainstorefilters/:filter", (req, res) => {
+  const { filter } = req.params;
+  res.setHeader("Content-Type", "application/json");
+  MainStore_Model.find({ gender: filter })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// Database CRUD Operations
+// @POST Request to GET the Product Details
+// GET
+router.get("/getproductitemdetails/:id", (req, res) => {
+  const { id } = req.params;
+  res.setHeader("Content-Type", "application/json");
+  MainStore_Model.find({ _id: id })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+// Database CRUD Operations
+// @POST Request to GET the People
+// GET
+router.get("/getallproductsapifilters/:filter", (req, res) => {
+  const { filter } = req.params;
+  res.setHeader("Content-Type", "application/json");
+  Products_Model.find({ Gender: filter })
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).json(`Error: ${err}`));
+});
+
+
+
+
+module.exports = router;
