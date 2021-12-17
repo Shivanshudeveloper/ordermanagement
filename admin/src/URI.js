8bc72a787ddd2bc9a1b86556119761cda3dad58f
Shivanshudeveloper <developer.shivanshu@gmail.com>
2021-12-17 11:15:05 +0530

    Work

--- admin/src/URI.js
+++ admin/src/URI.js
@@ -1,4 +1,7 @@
-const API_SERVICE = 'http://localhost:5000';
+// const API_SERVICE = 'http://localhost:5000';
+// const APP_URL = 'http://localhost:3000';
 
-const APP_URL = 'http://localhost:3000';
+const API_SERVICE = "https://ordermanagementapi.herokuapp.com";
+const APP_URL = "https://ordermanagement.vercel.app";
+
 export { APP_URL, API_SERVICE };
